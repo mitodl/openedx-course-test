11686945f9274a119f5984283eba426e3f9fb148
Carson Gee <x@carsongee.com>
2015-01-30 16:56:45 -0500

    Check all `<img>` tags in course data for alt attributes closes #11

--- test_course/py_course_test/__main__.py
+++ test_course/py_course_test/__main__.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys
 
-from import_test import import_course
+from import_test import test_course
 
 
 if __name__ == '__main__':
-    import_course(sys.argv[1])
+    test_course(sys.argv[1])
