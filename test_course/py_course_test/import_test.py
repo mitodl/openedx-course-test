82ea98f9d3481f168e9322214047ac1e7fdbd4e4
Carson Gee <x@carsongee.com>
2015-01-30 12:38:00 -0500

    Before failing image existence try and replace default images_course_image.jpg with images/course_image.jpg Also correct aspect ratio integer division mistake

--- test_course/py_course_test/import_test.py
+++ test_course/py_course_test/import_test.py
@@ -10,7 +10,7 @@ and print out a nice report of the results.
   This needs to be run from wihtin the ``edx-platform``
   directory to function properly.
 """
-from __future__ import print_function
+from __future__ import print_function, division
 from cStringIO import StringIO
 import codecs
 import logging
@@ -60,13 +60,19 @@ def check_image(directory, course):
     image_path = os.path.join(
         directory, course.data_dir, 'static', course.course_image
     )
-
     result = TestResult("Course image file exists", True)
     results.append(result)
     if not os.path.isfile(image_path):
-        result.result = False
-        result.value = 'no file at {}'.format(image_path)
-        return results
+        # Handle that lovely default of images_course_image.jpg problem
+        # by trying the _ to folder expansion
+        if course.course_image == 'images_course_image.jpg':
+            image_path = os.path.join(
+                directory, course.data_dir, 'static', 'images/course_image.jpg'
+            )
+            if not os.path.isfile(image_path):
+                result.result = False
+                result.value = 'no file at {}'.format(image_path)
+                return results
 
     result = TestResult("Course image is an image", True)
     results.append(result)
